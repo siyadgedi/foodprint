27e8fa095833590af61adc8e8785f6a141550498
20pricem <20pricem@gmail.com>
2023-12-02 19:22:48 -0600

    updated. works on sample data now, api still todo

--- percentages.py
+++ percentages.py
@@ -1,40 +1,102 @@
+import pandas as pd
 import numpy as np
-from scipy.optimize import linprog
+import matplotlib.pyplot as plt
+import json
+import torch
+import torch.nn.functional as F
+
+from collections import OrderedDict
+from torch.autograd import Variable
+from IPython.core.display import display, HTML
+
+nutritional_components = [
+ 'protein (g)',
+ 'sodium (g)',
+ 'carbohydrate (g)',
+ 'sugars (g)',
+ 'fat (g)',
+ 'calories',
+ 'saturated fat (g)'
+]
+
+food_composition = {
+    "sugar": {'protein (g)': 0, 'sodium (g)': 0, 'carbohydrate (g)': 100, 'sugars (g)': 100, 'fat (g)': 0, 'calories': 387, 'saturated fat (g)': 0},
+    "palm_oil": {'protein (g)': 0, 'sodium (g)': 0, 'carbohydrate (g)': 0, 'sugars (g)': 0, 'fat (g)': 100, 'calories': 865.8, 'saturated fat (g)': 47.8},
+    "hazelnut": {'protein (g)': 15.4, 'sodium (g)': 0, 'carbohydrate (g)': 17.8, 'sugars (g)': 5, 'fat (g)': 64.3, 'calories': 653.3, 'saturated fat (g)': 4.6},
+    "low_fat_cocoa": {'protein (g)': 20, 'sodium (g)': 0, 'carbohydrate (g)': 60, 'sugars (g)': 0, 'fat (g)': 10, 'calories': 420, 'saturated fat (g)': 0},
+    "nonfat_milk_powder": {'protein (g)': 36.9, 'sodium (g)': 0.57, 'carbohydrate (g)': 57.9, 'sugars (g)': 44.8, 'fat (g)': 47.4, 'calories': 842.7, 'saturated fat (g)': 21.1},
+    "nutella": {'protein (g)': 5.4, 'sodium (g)': 0.04, 'carbohydrate (g)': 62.1, 'sugars (g)': 54, 'fat (g)': 29.7, 'calories': 540, 'saturated fat (g)': 29.7}
+}
 
 
-## FYI: This is not working yet 
+ingredients = ["sugar", "palm_oil", "hazelnut", "low_fat_cocoa", "nonfat_milk_powder"]
+ingredient_weights = [None]*len(ingredients) #if we have info on the weight we can add it here
+target = "nutella"
 
-# sample data
-ingredients = ["sugar", "palm oil", "hazelnuts", "skim milk powder", "cocoa"]
-big7key = ["energy in kcal", "fat", "saturated fat", "carbohydrates", "sugar", "protein", "salt"]
+ig_weights = OrderedDict(zip(ingredients, ingredient_weights))
 
-# Nutritional values for each ingredient
-sugar_big7 = [387, 0, 0, 100, 100, 0, 0]
-palm_oil_big7 = [857, 100, 47.9, 0, 0, 0, 0]
-hazelnuts_big7 = [653.3, 64.3, 4.6, 17.8, 5, 13.5, 0]
-skim_milk_powder_big7 = [357, 0.7, 0.5, 51.5, 51.5, 35.3, 0.54]
-cocoa_big7 = [420, 10, 0, 60, 0, 20, 0]
+# Formulate the problem as an optimization task:
+# - linear model without bias
+# - one training sample per nutritional component (proteins, carbs, etc.)
+# - each sample Xi from X is a row vector containing the percentage of a given nutritional component in each ingredient
+# - each output Yi from Y is a scalar corresponding to the total amount of this nutrinional component in the final product
+# - after training, the weights are the amount of unknown ingredients in grams
 
-# nutritional values for main product
-big7 = [541, 29.7, 10.8, 59.5, 56.8, 5.41, 0.041]
+X = torch.zeros(len(nutritional_components), len(ingredients))
+W = torch.zeros(len(ingredients), 1)
+Y = torch.zeros(len(nutritional_components), 1)
 
-data = [sugar_big7, palm_oil_big7, hazelnuts_big7, skim_milk_powder_big7, cocoa_big7]
+for i, nutritional_component in enumerate(nutritional_components):
+    for j, ingredient in enumerate(ingredients):
+        X[i,j] = float(food_composition[ingredient][nutritional_component])
+    Y[i,0] = food_composition[target][nutritional_component]
 
-c = np.zeros(len(data))
+# initializing weights with what we know
+# for i, ingredient in enumerate(ingredients):
+#     if ingredient_weights[i] is not None:
+#         W[i,0] = ingredient_weights[i]
 
-A_ub = np.array(data).T
-b_ub = np.array(big7)
 
-# Equality constraint to ensure the sum of percentages is 1
-A_eq = np.ones((1, len(data)))
-b_eq = 1.0
+# normalization to be more efficient on smaller nutritional amounts
+Y_scaler = Y.clone()
+X.div_(Y_scaler)
+_ = Y.div_(Y_scaler)
 
-# Bounds for linear programming
-bounds = [(0, 1) for _ in range(len(data))] 
+X = Variable(X, requires_grad=False)
+W = Variable(W, requires_grad=True)
+Y = Variable(Y, requires_grad=False)
 
-result = linprog(c, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method='highs')
+epochs = 5000
+loss_history = np.zeros((epochs))
+for epoch in range(epochs):
+    Y_pred = X.mm(W)
+    
+    # l2 loss
+    loss = (Y_pred - Y).pow(2).sum()
+    
+    # try to go and stay at 100g total
+    loss += (W.sum()-1.).abs()*5
+    
+    loss_history[epoch] = loss.item()
+    loss.backward()
 
-ingredient_composition = result
+    for i in range(W.size(0)):
+        if ingredient_weights[i] is None: # update only unknown quantities
+            W.data[i].sub_(1e-5 * W.grad[i].data)
+            
+            # keep mass positives
+            W.data[i].clamp_(0., 100.)
+            
+            # keep ingredients in order
+            upper_lim = 100. if i==0 else W.data[i-1,0]
+            lower_lim = 0. if (i+1)==len(W) else W.data[i+1,0]
+            
+            W.data[i].clamp_(lower_lim, upper_lim)
+            
+    W.grad.data.zero_()
 
-print("\nIngredient Composition:")
-print(ingredient_composition)
+weights = OrderedDict([(ingredients[i], "%.1fg" % (W.data[i,0]*100)) for i in range(len(ingredients))])
+print("estimated composition for %s =" % target, json.dumps(weights, indent=2))
+print("total: %.1fg" % np.sum([W.data[i,0]*100 for i in range(len(ingredients))]))
+
+print("final loss: %.1f" % loss.item())
